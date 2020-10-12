68db326a588532bf9db14f62c86df37fcbd12a9c
Josephine Han <jsphnhan@gmail.com>
2020-10-12 13:56:18 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import list from './list'
+import '.App.css';
+import { render } from '@testing-library/react';
+
+
+class App extends Component {
+  static defaultProps = {
+    store: {
+      lists: [],
+      allCards: {},
+    }
+  };
+  
+  render() {
+    const { store } = this.props
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {store.lists.map(list => (
+            <List
+              key={list.id}
+              header={list.header}
+              cards={list.cardIds.map(id => store.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
+
+
